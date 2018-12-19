da495eccc22ef4d89094b42ca60c673b63243894
almarray <43389974+almarray@users.noreply.github.com>
2018-12-19 17:12:11 +0100

    Delete urls.py
    double file

--- polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:pompe_id>/', views.log, name='log'),
-]
